f0dca780fe4fe5f16e6e255a5f02c13f55de4f4a
Oleksandr <sashavytvyckyj@gmail.com>
2019-12-26 17:15:30 +0200

    03

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-/* const Telegraf = require('telegraf');
-const app = new Telegraf('1013712671:AAF0AzZ0zGawEY6X11SkGfXU5C_S70ld6lI');
+const TOKEN = process.env.TELEGRAM_TOKEN || '1013712671:AAF0AzZ0zGawEY6X11SkGfXU5C_S70ld6lI';
+const TelegramBot = require('node-telegram-bot-api');
+const options = {
+  webHook: {
+    port: process.env.PORT
+  }
+};
 
+const url = process.env.APP_URL || 'https://li1n-bot.herokuapp.com:443';
+const bot = new TelegramBot(TOKEN, options);
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
-const count = 0;
-
-app.on('text', function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
-    var resp = match[1]; // Получаем текст после /echo
-    app.sendMessage(fromId, resp);
-});
-  
-   app.startPolling(); */
-
-
-
-var TelegramBot = require('node-telegram-bot-api');
 var token = '1013712671:AAF0AzZ0zGawEY6X11SkGfXU5C_S70ld6lI';
 
 var bot = new TelegramBot(token, { polling: true });
