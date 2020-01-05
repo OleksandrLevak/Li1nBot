b59d63cb6c2fee77366c37f0b6e37ac5df93a1db
Oleksandr <sashavytvyckyj@gmail.com>
2020-01-05 12:52:19 +0200

    07.1

--- index.js
+++ index.js
@@ -29,26 +29,6 @@ bot.onText(/res/, function (msg) {
     bot.sendMessage(fromId, 'Всього віджався: ' + result);
 });
 
-var notes = [];
-
-bot.onText(/remaind me (.+) в (.+)/, function (msg, match) {
-    var userId = msg.from.id;
-    var text = match[1];
-    var time = match[2];
-
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
-
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++) {
-    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
-    if (notes[i]['time'] === curDate) {
-      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-      notes.splice(i, 1);
-    }
-  }
-}, 1000);
+setInterval(() => bot.sendMessage('Всього віджався: ' + result), 1000);
 
 
