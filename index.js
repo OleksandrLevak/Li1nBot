e08bfe47cc2044a9b7104f4f07a5412f92513c18
Oleksandr <sashavytvyckyj@gmail.com>
2020-01-12 08:38:46 +0200

    07.02

--- index.js
+++ index.js
@@ -29,5 +29,11 @@ bot.onText(/res/, function (msg) {
          bot.sendMessage(fromId, 'Всього віджався: ' + result);
 });
 
+bot.onText(/\/curse/, function (msg) {
+  var fromId = msg.from.id; // Получаем ID отправителя
+      
+       bot.sendMessage(fromId, 'test');
+});
+
 
 
