a63f38654ab9c27a929c8539b63ff9db0354ee45
Oleksandr <sashavytvyckyj@gmail.com>
2020-04-22 19:05:47 +0300

    correct

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TOKEN = process.env.TELEGRAM_TOKEN || '1013712671:AAF0AzZ0zGawEY6X11SkGfXU5C_S70ld6lI';
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
-const debug = require('./helpers.js');
+const debug = require('./helpers');
 const options = {
   webHook: {
     port: process.env.PORT
@@ -13,65 +13,65 @@ const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
-bot.on('message', msg => { 
-    const { id } = msg.chat; 
-    bot.sendMessage(id, debug(msg));
+
+bot.onText(/res/, function (msg) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+         bot.sendMessage(fromId, debug(msg));
 });
 
-
-// bot.onText(/\/curse/, function (msg) {
-//   var fromId = msg.from.id; // Получаем ID отправителя
+bot.onText(/\/curse/, function (msg) {
+  var fromId = msg.from.id; // Получаем ID отправителя
       
-//        bot.sendMessage(fromId, 'Яка валюта вас цікавить?', {
-//          reply_markup: {
-//            inline_keyboard: [
-//              [
-//                {
-//                  text: '€ - EUR',
-//                  callback_data: 'EUR'
-//                },
-//                {
-//                 text: '$ - USD',
-//                 callback_data: 'USD'
-//               },
-//               {
-//                 text: '₿ - BTC',
-//                 callback_data: 'BTC'
-//               },
-//               {
-//                 text: '₽ - RUR',
-//                 callback_data: 'RUR'
-//               },
+       bot.sendMessage(fromId, 'Яка валюта вас цікавить?', {
+         reply_markup: {
+           inline_keyboard: [
+             [
+               {
+                 text: '€ - EUR',
+                 callback_data: 'EUR'
+               },
+               {
+                text: '$ - USD',
+                callback_data: 'USD'
+              },
+              {
+                text: '₿ - BTC',
+                callback_data: 'BTC'
+              },
+              {
+                text: '₽ - RUR',
+                callback_data: 'RUR'
+              },
 
-//              ]
-//            ]
-//          }
-//        });
-// });
+             ]
+           ]
+         }
+       });
+});
 
-// bot.on('callback_query', query => {
-//   const id = query.message.chat.id;
+bot.on('callback_query', query => {
+  const id = query.message.chat.id;
 
-//   request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', 
-//   function(error, response, body){
-//     const data = JSON.parse(body);
-//     const result = data.filter(item => item.ccy === query.data)[0];
-//     const flag = {
-//       'EUR': '🇪🇺',
-//       'USD': '🇺🇸',
-//       'RUR': '🇷🇺',
-//       'BTC': '₿',
-//       'UAH': '🇺🇦',
-//     }
-//     let md = `
-//       *${flag[result.ccy]} ${result.ccy} 💱 ${result.base_ccy} ${flag[result.base_ccy]}*
+  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', 
+  function(error, response, body){
+    const data = JSON.parse(body);
+    const result = data.filter(item => item.ccy === query.data)[0];
+    const flag = {
+      'EUR': '🇪🇺',
+      'USD': '🇺🇸',
+      'RUR': '🇷🇺',
+      'BTC': '₿',
+      'UAH': '🇺🇦',
+    }
+    let md = `
+      *${flag[result.ccy]} ${result.ccy} 💱 ${result.base_ccy} ${flag[result.base_ccy]}*
 
-//       Buy:  ${result.buy}
-//       Sale: ${result.sale}
-//     `;
-//     bot.sendMessage(id, md, {parse_mode: 'Markdown'});
-//   }
-//   );
-// })
+      Buy:  ${result.buy}
+      Sale: ${result.sale}
+    `;
+    bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+  }
+  );
+})
 
 
