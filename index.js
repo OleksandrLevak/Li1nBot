b4c1ee19fc68ada6cc0f846678a604551275e8e8
Oleksandr <sashavytvyckyj@gmail.com>
2020-04-22 18:50:39 +0300

    correct

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TOKEN = process.env.TELEGRAM_TOKEN || '1013712671:AAF0AzZ0zGawEY6X11SkGfXU5C_S70ld6lI';
 const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
 const options = {
   webHook: {
     port: process.env.PORT
@@ -10,12 +11,12 @@ const url = process.env.APP_URL || 'https://li1n-bot.herokuapp.com:443';
 const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
-const request = require('request');
 
 
-// bot.on('message', msg => {  
-//     bot.sendMessage(msg.from.id, `Hello ${from.first_name}`);
-// });
+
+bot.onText('hello', msg => {  
+    bot.sendMessage(msg.from.id, `Hello ${from.first_name}`);
+});
 
 
 bot.onText(/\/curse/, function (msg) {
