b25cf61f4221a53c66ea4dd87db149f7c6acca31
Oleksandr <sashavytvyckyj@gmail.com>
2020-05-10 22:27:37 +0300

    update

--- helpers.js
+++ helpers.js
@@ -13,20 +13,12 @@
         const subFlag = source.substr(3, source.length);
         const arrOfInfo = subFlag.split('. ');
 
+        arrOfInfo.forEach(word => word.charAt(0).toUpperCase() + word.slice(1));
+
         const arrOfCapInfo = getCapitalizedWords(arrOfInfo);
 
         const description = `${arrOfCapInfo[0]}\nВиробництво: ${arrOfCapInfo[1]}\nРозмір: ${arrOfCapInfo[2]}\nЦіна: ${arrOfCapInfo[3]} грн.\n№ ${arrOfCapInfo[4]}\n`
         return description;
     },
-
-    getCapitalizedWords(arrOfinfo){
-        const arrOfCapInfo = []
-
-        for(word of arrOfinfo){
-           const wordCap = word.charAt(0).toUpperCase() + word.slice(1);
-           arrOfCapInfo.push(wordCap);
-        }
-        return arrOfinfo;
-    }
  }
 
