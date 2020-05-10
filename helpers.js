74e98e3592341dab51d2f0b810c12f933d7b5467
Oleksandr <sashavytvyckyj@gmail.com>
2020-05-10 22:30:35 +0300

    update

--- helpers.js
+++ helpers.js
@@ -13,7 +13,7 @@
         const subFlag = source.substr(3, source.length);
         const arrOfInfo = subFlag.split('. ');
 
-        arrOfInfo.forEach(word => word.charAt(0).toUpperCase() + word.slice(1));
+        //arrOfInfo.forEach(word => word.charAt(0).toUpperCase() + word.slice(1));
 
         const description = `${arrOfInfo[0]}\nВиробництво: ${arrOfInfo[1]}\nРозмір: ${arrOfInfo[2]}\nЦіна: ${arrOfInfo[3]} грн.\n№ ${arrOfInfo[4]}\n`
         return description;
