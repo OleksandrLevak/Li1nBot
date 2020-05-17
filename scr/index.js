e3db0828a9e4c4d09fda2bf11bf0a869ef061e87
Oleksandr <sashavytvyckyj@gmail.com>
2020-05-17 19:51:02 +0300

    update

--- scr/index.js
+++ scr/index.js
@@ -2,7 +2,11 @@ const TOKEN = process.env.TELEGRAM_TOKEN || '1013712671:AAF0AzZ0zGawEY6X11SkGfXU
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const helpers = require('../helpers');
-
+const options = {
+    webHook: {
+        port: process.env.PORT
+    }
+};
 
 const url = process.env.APP_URL || 'https://li1n-bot.herokuapp.com:443';
 const bot = new TelegramBot(TOKEN, options);
